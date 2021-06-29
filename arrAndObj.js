5403d838bd9f1baa6830cda0ae0fd98b8ed51367
sofialay <sofi-lay@hotmail.com>
2021-06-28 21:36:07 -0300

    obj and arrays

--- arrAndObj.js
+++ arrAndObj.js
@@ -15,3 +15,27 @@ const unique = Array.from(new Set(users));
 console.log(unique)
 
 //output: ["Pam", "John", "Dwight", "Michael", "Jim"]
+
+// Array to object
+
+const arr = [
+    "Pam",
+    "John",
+    "Dwight",
+    "Michael",
+    "Jim",
+    "Jim",
+]
+
+const arrToObj = {...arr}
+
+// Object to array
+
+const users = {
+    name: 'Sofia',
+    ocupation: 'Front End Dev',
+    hobbies: 'taking photos'
+}
+
+const toArr = Object.keys(users) //output: ['name', 'ocupation', 'hobbies']
+const toArr = Object.values(users) //output: ['Sofia', 'Front End Dev', 'taking photos']
